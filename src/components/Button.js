fc0a804bd80f63e75bed581daabe5b08cd753b50
Kristy Gislason <kristygislason@gmail.com>
2023-01-23 12:59:57 -0800

    Set-up Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,19 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+   let buttonDisabled = "";
+   let buttonOnClick = (event) => {
+      console.log('Button clicked');
+   };
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+  
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return <button disabled={props.disabled} onClick={props.onClick} className={buttonClass}>{props.children}</button>;
 }
