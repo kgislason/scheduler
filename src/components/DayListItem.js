7a6fed89c337f51108ab7963ce7c527ff7cb9331
Kristy Gislason <kristygislason@gmail.com>
2023-01-23 14:32:23 -0800

    Add tests for DayListItem components and add cases to handle 1 spot reminaing and no spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,13 +10,29 @@ export default function DayListItem(props) {
     'day-list__item--full': (props.spots === 0)
   });
 
+  const formatSpots = function() {
+    if (props.spots > 1) {
+      return  `${props.spots} spots remaining`;
+    }
+
+    if (props.spots === 1) {
+      return  `${props.spots} spot remaining`;
+    }
+
+    if (props.spots < 1) {
+      return 'no spots remaining';
+    }
+  }
+
   return (
     <li 
       onClick={() => props.setDay(props.name)}
       className={dayClass}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">
+        {formatSpots()}        
+      </h3>
     </li>
   );
 }
