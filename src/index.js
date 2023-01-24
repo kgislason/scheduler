ae7bbda743d3144562decc5586cb6e468010725d
Kristy Gislason <kristygislason@gmail.com>
2023-01-24 11:45:52 -0800

    Change import to consistent referencing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
-import Application from "components/Application";
+import Application from "./components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
