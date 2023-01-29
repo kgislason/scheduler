300148ca3250273206a65542e54814988ddc0c5a
Kristy Gislason <kristygislason@gmail.com>
2023-01-28 22:12:35 -0800

    Builc out Appointment componenet and stories

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,13 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,5 +152,57 @@ storiesOf("InterviewerList", module)
     .add("Appointment with Time", () => (
       <Appointment
         time="12pm"
+      />
+    ))
+    .add("Header", () => (
+      <Header
+        time="12pm"
+      />
+    ))
+    .add("Empty", () => (
+      <Empty 
+        onAdd={ action("onAdd")}
+      />
+    ))
+    .add("Show", () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={ action("onEdit") }
+        onDelete={ action("onDelete") }
+      />
+    ))
+    .add("Confirm", () => (
+      <Confirm 
+        message="Delete the appointment?"
+        onConfirm={ action("onConfirm") }
+        onCancel={ action("onCancel") }
+      />
+    ))
+    .add("Status", () => (
+      <Status 
+        message="Deleting"
+      />      
+    ))
+    .add("Error", () => (
+      <Error
+        message="Could not delete appointment."
+        onClose={ action("onClose") }
+      />
+    ))
+    .add("Form Create", () => (
+      <Form
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={ action("onCancel")}
+      />
+    ))
+    .add("Form Edit", () => (
+      <Form
+        student="Kristy Gislason"
+        interviewer={1}
+        interviewers={interviewers}
+        onSave={ action("onSave")}
+        onCancel={ action("onCancel") }
       />
     ));
