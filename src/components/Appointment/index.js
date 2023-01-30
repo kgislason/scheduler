9e2e569edc63313224856a43312ac77f4b92c3b6
Kristy Gislason <kristygislason@gmail.com>
2023-01-30 13:58:51 -0800

    Add parts of appointment to Appointment componeents including Header, Show and Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import './styles.scss';
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
+  console.log("Interview",  props.interview);
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+        <Header 
+          time="5pm"
+        />
+        { (props.interview) && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+        { (! props.interview) && <Empty />}
+    </article>
   )
 }
