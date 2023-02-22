30d1e1322b47f3eec5a9cb8601d71ed9df1b6b1f
Kristy Gislason <kristygislason@gmail.com>
2023-02-22 11:39:36 -0800

    Update axios mock test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,13 +7,13 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Application", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
-
-    await waitForElement(() => getByText("Monday"));
-
-    //fireEvent.click(getByText("Tuesday"));
-
-    //expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  
+    return waitForElement(() => getByText("Monday"))
+      .then(() => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      });
   });
 });
