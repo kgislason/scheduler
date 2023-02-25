7c7de6c4b5db90bd5878b90f0bf1d91ecd1450e4
Kristy Gislason <kristygislason@gmail.com>
2023-02-24 16:11:40 -0800

    Add cypress tests for appointment add edit and delete

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,64 +1,59 @@
 describe("Appointments", () => {
-  it.skip("should book an interview", () => {  
-    cy.request("http://localhost:8001/api/debug/reset");
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
+   });
 
-    // Visits the root of our web server
-    cy.visit("http://localhost:8000/");
+  it("should book an interview", () => {
 
-    // Clicks on the "Add" button in the second appointment
-    cy.contains("[data-testid=appointment]", "1pm")
-      .find('.appointment__add-button')
-      .click().then(() => {
-        // Enters their name
+    cy.get("[alt=Add]")
+      .first()
+      .click();
 
-        cy.get('input').type('Kristy Gislason');
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
 
-        // Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
 
-        cy.get("[data-testid=interviewer]").find("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
 
-        // Clicks the save button
-        cy.get('.button--confirm').click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
 
-        // Sees the booked appointment
-        cy.get("[data-testid=appointment] h2").should(($h2) => {
-          expect($h2.eq(1)).to.contain("Kristy Gislason");
-        });
-      });
-
-      cy.request("http://localhost:8001/api/debug/reset");
   });
 
-  // If we edit the existing appointment booked for "Archie Cohen", then we don't need to create an appointment first.
   it("should edit an interview", () => {
-    // Visits the root of our web server
-    cy.visit("/");
 
-    // Clicks the edit button for the existing appointment
-    cy.contains("[data-testid=appointment]", "12pm").then( ($appointment) => {
-      $appointment.find('.appointment__card').trigger("mouseover");
-    })
-      
-      // .find('img[alt="Edit"]')
-      // .click();
-    // Changes the name and interviewer
-    // Clicks the save button
-    // Sees the edit to the appointment   
-    
-    // Reset Database
-    cy.request("http://localhost:8001/api/debug/reset");
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+
   });
 
-  // We can also perform a test to cancel an existing interview. It is for this reason that we need to reset the database after each test. If one test cancels and interview and the next test expects that interview to exist, then our tests can break for reasons unrelated to our code quality.
-  it.skip("should cancel an interview", () => {
-    // Visits the root of our web server
-    cy.visit("/");
+  it("should cancel an interview", () => {
 
-    // Clicks the delete button for the existing appointment
-    // Clicks the confirm button
-    // Sees that the appointment slot is empty
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
 
-    // Reset Database
-    cy.request("http://localhost:8001/api/debug/reset");
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting...").should("exist");
+
+    cy.contains("Deleting...").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+
   });
 });
